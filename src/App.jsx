7bcae05489ed968209dd07f13d28125dbd1f7e80
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-01 21:07:46 +0300

    add mebel file basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Hom from './pages/Hom'
+import Shop from './pages/Shop'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout/>}>
+        <Route index element={<Hom/>}/>
+        <Route path='shop' element={<Shop/>}/>
+      </Route>
+    )
+  )
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
